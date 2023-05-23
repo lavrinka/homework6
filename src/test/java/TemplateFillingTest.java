453ff9298df68403c7c91fead829ea838a4e106f
Kseniia Lavrinovich <kseniia_lavrinovich@epam.com>
2023-05-23 17:08:11 +0200

    TemporaryFolder rule, Mock reading from file/console, Use partial mock, Use spy

--- src/test/java/TemplateFillingTest.java
+++ src/test/java/TemplateFillingTest.java
@@ -1,3 +1,4 @@
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -5,9 +6,12 @@ import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.epam.TemplateFilling;
 import org.epam.Value;
+import org.epam.read.ReadFromConsole;
+import org.epam.read.ReadFromFile;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
+import org.mockito.Mockito;
 
 public class TemplateFillingTest {
 
@@ -24,6 +28,31 @@ public class TemplateFillingTest {
               .isNotEmpty()
               .isEqualTo("This is template with name test. Insert #{value}");
   }
+
+  //Mock reading from file/console
+  @Test
+  void shouldParseTemplateWithValuesWithMockReadingFromConsole() {
+
+    ReadFromConsole readFromConsole = Mockito.mock(ReadFromConsole.class);
+    TemplateFilling templateFilling = new TemplateFilling();
+    templateFilling.setTemplate("This is template with name #{name}. Insert #{some value}");
+    Mockito.when(readFromConsole.readTemplateFromConsole()).thenReturn(templateFilling);
+
+    ArrayList<Value> values = new ArrayList<>();
+    values.add(new Value("#{name}", "test"));
+    values.add(new Value("#{some value}", "#{value}"));
+    Mockito.when(readFromConsole.readValuesFromConsole()).thenReturn(values);
+
+    readFromConsole.readTemplateFromConsole();
+
+    TemplateFilling templateFillingMock = readFromConsole.readTemplateFromConsole();
+    ArrayList<Value> valuesMock = readFromConsole.readValuesFromConsole();
+    String result = templateFillingMock.pasteValues(valuesMock);
+    Assertions.assertThat(result)
+              .isNotEmpty()
+              .isEqualTo("This is template with name test. Insert #{value}");
+  }
+
 
   //Dynamic tests + Implement meta annotations and filtering
   @Slow
@@ -67,6 +96,28 @@ public class TemplateFillingTest {
     Assertions.assertThat(result)
               .isNotEmpty()
               .isEqualTo("This is a template with values #{value} and 123");
+
+  }
+
+  //Use partial mock + Use spy
+  @Test
+  void shouldPasteValuesInTemplateWithLatinWithPartialMockOfReading() {
+    String inputFile = "src/test/resources/fileWithLatin.txt";
+    ReadFromFile readFromFile = Mockito.spy(new ReadFromFile(inputFile));
+
+    TemplateFilling templateFilling = readFromFile.readTemplateFromFile();
+
+    ArrayList<Value> values = new ArrayList<>();
+    values.add(new Value("#{val1}", new String("#{value}".getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1)));
+    values.add(new Value("#{val2}", new String("123Ñ".getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1)));
+
+    Mockito.doReturn(values).when(readFromFile).readValuesFromFile();
+    String message = new String("This is a templateÑ with values #{value} and 123Ñ".getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
+
+    String result = templateFilling.pasteValues(values);
+    Assertions.assertThat(result)
+              .isNotEmpty()
+              .isEqualTo(message);
 
   }
 }
--- src/test/java/WriteToFileTest.java
+++ src/test/java/WriteToFileTest.java
@@ -1,27 +1,29 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.epam.write.WriteToFile;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 public class WriteToFileTest {
 
   @Test
-  void shouldUseLatinForWrite() {
-    String outputFile = "src/test/resources/fileoutput_shouldUseLatinForWrite.txt";
-
+  void shouldUseLatinForWrite(@TempDir Path tempDir) {
+    Path outputFilePath = tempDir.resolve("fileoutput_shouldUseLatinForWrite.txt");
+    System.out.println(outputFilePath.toString());
     String message = new String("This is a template with values #{value} and 123Ñ".getBytes(StandardCharsets.UTF_8),
         StandardCharsets.ISO_8859_1);
-    WriteToFile writeToFile = new WriteToFile(outputFile);
+    WriteToFile writeToFile = new WriteToFile(outputFilePath.toString());
     writeToFile.writeMessageToFile(message);
 
     try {
-      Path outputPath = Paths.get("src/test/resources/fileoutput_shouldUseLatinForWrite.txt");
-      List<String> read = Files.readAllLines(outputPath);
+      assertTrue(Files.exists(outputFilePath));
+      List<String> read = Files.readAllLines(outputFilePath);
       Assertions.assertThat(read.size())
                 .isEqualTo(1);
       Assertions.assertThat(read.get(0))
