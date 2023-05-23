468143dedd9c5d0ae65b5e89f1940f2b255322a8
Kseniia Lavrinovich <kseniia_lavrinovich@epam.com>
2023-05-23 21:52:40 +0200

    Depends on number of arguments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,13 +8,20 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Try to send message");
 
-    SendServiceInConsole sendServiceInConsole = new SendServiceInConsole();
-    sendServiceInConsole.sendMessage();
-
-    SendServiceInFile sendServiceInFile = new SendServiceInFile();
-    String input = "src/test/resources/fileWithLatin.txt";
-    String output = "test.txt";
-    sendServiceInFile.sendMessage(input, output);
-
+    if (args.length == 2) {
+      String input = args[0];
+      String output = args[1];
+      /*
+        input = "src/test/resources/fileWithLatin.txt";
+        output = "test.txt";
+       */
+      SendServiceInFile sendServiceInFile = new SendServiceInFile();
+      sendServiceInFile.sendMessage(input, output);
+    } else if (args.length == 0) {
+      SendServiceInConsole sendServiceInConsole = new SendServiceInConsole();
+      sendServiceInConsole.sendMessage();
+    } else {
+      System.out.println("Wrong number of arguments");
+    }
   }
 }
